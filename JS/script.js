6462b1adab57b725c15e30be0234d240cf300750
Maike Castilho <maikecastilho18@gmail.com>
2023-01-30 16:29:54 -0300

    commit

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,12 @@
 function verifica(){
     var num = document.getElementById("numero").value;
   
+    if(num == 0){
+        alert("insira um número")
+        exit;
+    }
+    
+
     if (num % 2 == 0) {
         alert("O número lido é par!");
     } else {
@@ -8,7 +14,4 @@ function verifica(){
     }
   
 }
-
-
-
 
