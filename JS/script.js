751e34dd3c4fdc546f32d6b482f5cbfee0c4ac05
Maike Castilho <maikecastilho18@gmail.com>
2023-01-30 14:04:26 -0300

    commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+function verifica(){
+    var num = document.getElementById("numero").value;
+  
+    if (num % 2 == 0) {
+        alert("O número lido é par!");
+    } else {
+       alert("O número lido é impar!");
+    }
+  
+}
+
+
+
+
